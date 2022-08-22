896d83c11eef4e0543c29629ddcbe01c81cb628a
GitPit <rifflifter@gmail.com>
2022-08-22 16:34:06 -0400

    populate = () => {} for appendChild

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -4,5 +4,25 @@ hamburger = () => {
         let menu = document.createElement('ul');
         menu.setAttribute('id', 'menu-list');
         document.querySelector('#side').append(menu);
+
+        populate = () => {
+
+            let projects = [];
+
+            for (let element of projects) {
+                projects[element] = projects[element] + 1;
+                if (projects.textContent = "") {
+                    let projectName = projects.createElement('li');
+                }
+                
+            }
+
+
+            menu.appendChild(projectName);
+        }
+
+        populate();
     })
-}
+};
+
+hamburger();
