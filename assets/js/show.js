7ae55b416376e76743491e73317d311e389a5509
GitPit <rifflifter@gmail.com>
2022-08-26 15:32:23 -0400

    document.getElementsByClassName('sketch')

--- assets/js/show.js
+++ assets/js/show.js
@@ -3,3 +3,10 @@
 // }
 
 const sketch = document.querySelector("#sketch");
+sketch.addEventListener('click', (e => {
+    e.preventDefault();
+
+    console.log("sketch clicked");
+
+    document.getElementsByClassName('sketch').style.display = 'block';
+}))
